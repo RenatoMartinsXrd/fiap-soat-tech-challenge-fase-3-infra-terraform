36d1412622e9e1db499e813894bed1fcb7144cb5
Renato Martins <70973620+RenatoMartinsXrd@users.noreply.github.com>
2025-03-25 08:07:44 -0300

    Update security.tf

--- terraform-eks/security.tf
+++ terraform-eks/security.tf
@@ -28,6 +28,5 @@ resource "aws_security_group_rule" "allow_eks_30090" {
   to_port           = 30080
   protocol         = "tcp"
   security_group_id = aws_security_group.eks_sg.id
-  cidr_blocks = [module.vpc.vpc_cidr_block]
   source_security_group_id = aws_security_group.sg.id
 }
