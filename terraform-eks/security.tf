21952d9cdb733071c142c89c703e5a1c907bdeae
Renato Martins <70973620+RenatoMartinsXrd@users.noreply.github.com>
2025-03-25 08:03:22 -0300

    Update security.tf

--- terraform-eks/security.tf
+++ terraform-eks/security.tf
@@ -3,6 +3,20 @@ resource "aws_security_group" "eks_sg" {
   description = "Security Group fixo para o cluster EKS"
   vpc_id      = module.vpc.vpc_id
 
+  ingress {
+    from_port   = 30080
+    to_port     = 30080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Permite tráfego de qualquer origem
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "eks-cluster-sg"
   }
@@ -15,4 +29,5 @@ resource "aws_security_group_rule" "allow_eks_30090" {
   protocol         = "tcp"
   security_group_id = aws_security_group.eks_sg.id
   cidr_blocks = [module.vpc.vpc_cidr_block]
+  source_security_group_id = aws_security_group.sg.id
 }
